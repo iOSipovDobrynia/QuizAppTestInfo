fcb6ab79490885f98315d8e4490687e05bf6f55f
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-08-08 23:51:05 +0300

    work with singleStack

--- QuizAppTestInfo/ViewControllers/QuestionsViewController.swift
+++ QuizAppTestInfo/ViewControllers/QuestionsViewController.swift
@@ -8,9 +8,75 @@
 import UIKit
 
 class QuestionsViewController: UIViewController {
-
+    
+    @IBOutlet var questionProgressView: UIProgressView!
+    @IBOutlet var questionLabel: UILabel!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedSlider: UISlider!
+    @IBOutlet var rangedLabels: [UILabel]!
+    
+    private let questions = Question.getQuestions()
+    private var questionIndex = 0
+    private var answers: [Answer] {
+        questions[questionIndex].answers
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
+    }
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func multipleAnswerButtonPressed() {
+    }
+    @IBAction func rangedAnswerButtonPressed() {
+    }
+}
+
+//MARK: -Private methods
+extension QuestionsViewController {
+    private func updateUI() {
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden.toggle()
+        }
+        
+        let currentQuestion = questions[questionIndex]
+        
+        questionLabel.text = currentQuestion.title
+        
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        showCurrentQuestion(for: currentQuestion.responseType)
+    }
+    
+    private func showCurrentQuestion(for type: ResponseType) {
+        switch type {
+        case .single:
+            showSingleStackView(with: answers)
+        case .multiple:
+            break
+        case .ranged:
+            break
+        }
+    }
+    
+    private func showSingleStackView(with answers: [Answer]) {
+        singleStackView.isHidden.toggle()
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.title, for: .normal)
+        }
     }
 }
 
