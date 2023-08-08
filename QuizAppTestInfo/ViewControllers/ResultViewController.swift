fce0f8fb2a4e0548f6e8ca87360d0c0cab6ca22b
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-08-08 22:12:33 +0300

    add questionsVC and resultVC

---
+++ QuizAppTestInfo/ViewControllers/ResultViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ResultViewController.swift
+//  QuizAppTestInfo
+//
+//  Created by Goodwasp on 08.08.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
