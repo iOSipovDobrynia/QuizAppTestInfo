5995720ccca88da00880eb0a42a641d3d8ef3f9e
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-08-09 21:20:58 +0300

    add calculateResultGame func and showResultGame func

--- QuizAppTestInfo/ViewControllers/ResultViewController.swift
+++ QuizAppTestInfo/ViewControllers/ResultViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ResultViewController: UIViewController {
+    @IBOutlet var gameLabel: UILabel!
+    @IBOutlet var gameDescriptionLabel: UILabel!
     
     var answers: [Answer]!
     
@@ -15,11 +17,42 @@ class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
+        
+        showResultGame()
     }
     
     // MARK: - IB Actions
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         navigationController?.dismiss(animated: true)
+    }
+}
+
+//MARK: - Private func
+extension ResultViewController {
+    private func calculateMostFrequenceGame(from answers: [Answer]) -> Game {
+        let games = answers.map { $0.game }
+        
+        var countOfGames: [Game: Int] = [:]
+        
+        games.forEach { game in
+            guard let count = countOfGames[game] else {
+                countOfGames.updateValue(1, forKey: game)
+                return
+            }
+            countOfGames.updateValue(count + 1, forKey: game)
+        }
+        
+        guard let mostFrequenceGame = countOfGames.sorted(by: { $0.value > $1.value }).first?.key else {
+            return .alias
+        }
+        
+        return mostFrequenceGame
+    }
+    
+    private func showResultGame() {
+        let resultGame = calculateMostFrequenceGame(from: answers)
+        gameLabel.text = "Игра – \(resultGame.rawValue)"
+        gameDescriptionLabel.text = resultGame.definition
     }
     
 }
